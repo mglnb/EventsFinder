f8ef8ba97260a14960b324e9bcbdae09a0dac169
Miguel Boanova <miguel.boanova@prodigious.com>
2017-12-28 13:05:53 -0200

    Adicionado event

---
+++ src/components/screens/Event.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {Text} from 'react-native'
+export default class Event extends Component {
+    state = {  }
+    render() {
+        return (
+            <Text> TODO </Text>
+        );
+    }
+}

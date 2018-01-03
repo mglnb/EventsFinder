7c63206a8e151947281b41a6e178bc50054165e1
Miguel Boanova <mgl.deadly@gmail.com>
2018-01-03 15:28:57 -0200

    Adicionado botão scrollToTop para ListView

--- src/components/screens/ListEvents.js
+++ src/components/screens/ListEvents.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react'
 import { Text, View, StyleSheet, FlatList, ActivityIndicator } from 'react-native'
 import BoxEvents from '../sharedComponents/BoxEvents'
 import _ from 'lodash'
+import Touchable from '../sharedComponents/Touchable'
+import { Icon } from 'react-native-elements';
 export default class ListEvents extends Component {
   constructor (props) {
     super(props)
     this.state = {
       events: ['1'],
-      eventsFiltered: [], 
+      eventsFiltered: [],
       filter: '',
       isLoading: false,
       haveFilter: false
@@ -35,9 +37,11 @@ export default class ListEvents extends Component {
         isLoading: false
       }))
       .then(() => {
+        console.log(this.state.events)
         this.setState({
-          events: this.state.events.filter((value,index) => new Date(value.startTime)) > new Date()
+          events: this.state.events.filter((value, index) => new Date(value.startTime) > new Date())
         })
+        console.log(this.state.events)
       })
       .then(() => {
         let haveFilter = this.props.navigation.state && this.props.navigation.state.params && this.props.navigation.state.params.filter || null
@@ -55,6 +59,9 @@ export default class ListEvents extends Component {
   isLoading () {
     return this.state.isLoading ? <ActivityIndicator size="large" style={styles.activity} color="#0000ff" /> : undefined
   }
+  scrollToTop () {
+    this.flatListRef.scrollToIndex({ animated: true, index: 0 })
+  }
 
   render () {
     let events = this.state.haveFilter ? this.state.eventsFiltered : this.state.events
@@ -62,7 +69,18 @@ export default class ListEvents extends Component {
       if (!this.isLoading()) {
         return (
           <View>
-            <FlatList data={this.state.haveFilter ? this.state.eventsFiltered : this.state.events}
+            <View style={styles.button}>
+              <Touchable
+                background={Touchable.Ripple('#fff', true)}
+                style={styles.colorButtonContainer}
+                onPress={() => this.scrollToTop()}>
+
+                <Icon name="arrow-upward" raised reverse color={'#5480F3'} />
+              </Touchable>
+            </View>
+            <FlatList
+              data={this.state.haveFilter ? this.state.eventsFiltered : this.state.events}
+              ref={(ref) => { this.flatListRef = ref; }}
               keyExtractor={(item, index) => item.id}
               renderItem={({ item }) => <BoxEvents navigation={this.props.navigation} value={item} key={item.id} />} />
           </View>
@@ -75,6 +93,17 @@ export default class ListEvents extends Component {
 }
 
 const styles = StyleSheet.create({
+  button: {
+    alignSelf: 'flex-end',
+    position: 'absolute',
+    bottom: 25,
+    right: 15,
+    zIndex: 9999,
+  },
+  colorButtonContainer: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
   textError: {
     fontSize: 24,
     flex: 1,
